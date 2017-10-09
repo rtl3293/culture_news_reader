4f1f0eb426782b739555da6314574a17444103b0
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-09 19:58:23 -0400

    Added categories user can choose from

--- lib/culture_news_reader/cli.rb
+++ lib/culture_news_reader/cli.rb
@@ -2,6 +2,11 @@ class CultureNewsReader::CLI
 
 	def call
 		puts "The Fader"
+		puts "What category would you like to see?"
+		categories = ["Music", "Style", "Culture"]
+		categories.each_with_index do |category, i|
+			puts "#{i+1}. #{category}"
+		end
 	end
 
 end
