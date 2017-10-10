2cb6a146049758bd547b565b9dac3bb127316326
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-09 20:14:30 -0400

    streamlined interface code

--- lib/culture_news_reader/cli.rb
+++ lib/culture_news_reader/cli.rb
@@ -1,18 +1,23 @@
 class CultureNewsReader::CLI
 
-	attr_accessor :category
-
 	def call
-		puts "The Fader"
+		list_categories
+		menu
 	end
 
-	def menu
+	def list_categories
+		puts "The Fader"
 		categories = ["Music", "Style", "Culture"]
-		puts "Enter the number for the news you'd like:"
 		categories.each_with_index do |category, i|
 			puts "#{i+1}. #{category}"
 		end
-		@category = gets.strip
 	end
+
+	def menu	
+		puts "Enter the number for the news you'd like:"
+		category = gets.strip
+	end
+
+
 
 end
