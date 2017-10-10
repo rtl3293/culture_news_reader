8e426d1e67a20a59597ab0e82d461a22023e368c
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-09 20:06:43 -0400

    finished menu and user input for choosing category

--- lib/culture_news_reader/cli.rb
+++ lib/culture_news_reader/cli.rb
@@ -1,5 +1,7 @@
 class CultureNewsReader::CLI
 
+	attr_accessor :category
+
 	def call
 		puts "The Fader"
 	end
@@ -10,6 +12,7 @@ class CultureNewsReader::CLI
 		categories.each_with_index do |category, i|
 			puts "#{i+1}. #{category}"
 		end
+		@category = gets.strip
 	end
 
 end
