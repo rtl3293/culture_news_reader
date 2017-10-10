fd5036ed9681b451f170c1917f67dce6fb079bc6
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-09 22:07:02 -0400

    created Scraper class methods base

--- lib/culture_news_reader/cli.rb
+++ lib/culture_news_reader/cli.rb
@@ -14,15 +14,20 @@ class CultureNewsReader::CLI
 	end
 
 	def menu
+		fader_url = 'http://www.thefader.com/'
 		input = nil	
 		while input != "exit"
 			puts "Enter the number for the news you'd like:"
 			input = gets.strip.downcase
 			case input
 			when "1"
-				puts "Music News..."
+				#puts "Music News..."
+				Scraper.scrape_music_page(fader_url + 'music')
 			when "2"
-				puts "Style News..."
+				#puts "Style News..."
+				Scraper.scrape_style_page(fader_url + 'style')
+			when "3"
+				Scraper.scrape_culture_page(fader_url + 'culture')
 			when "list"
 				list_categories
 			end
--- lib/culture_news_reader/scraper.rb
+++ lib/culture_news_reader/scraper.rb
@@ -1,15 +1,20 @@
+
+
 class Scraper
 
 	def self.scrape_music_page(music_url)
-
+		html = open(music_url)
+		doc = Nokogiri::HTML(html)
 	end
 
 	def self.scrape_style_page(style_url)
-
+		html = open(style_url)
+		doc = Nokogiri::HTML(html)
 	end
 
 	def self.scrape_culture_page(culture_url)
-
+		html = open(culture_url)
+		doc = Nokogiri::HTML(html)
 	end
 
 end
