aca4ff05f9221968a4586cb386acf5765e3092eb
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-09 20:04:14 -0400

    added menu to CLI

--- lib/culture_news_reader/cli.rb
+++ lib/culture_news_reader/cli.rb
@@ -2,8 +2,11 @@ class CultureNewsReader::CLI
 
 	def call
 		puts "The Fader"
-		puts "What category would you like to see?"
+	end
+
+	def menu
 		categories = ["Music", "Style", "Culture"]
+		puts "Enter the number for the news you'd like:"
 		categories.each_with_index do |category, i|
 			puts "#{i+1}. #{category}"
 		end
