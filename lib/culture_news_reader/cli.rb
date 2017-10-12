0a86651951d61e2f0907a92266b9bcf0c763ed5a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-12 15:45:21 -0400

    moved output formatting to it's own #display method

--- lib/culture_news_reader/cli.rb
+++ lib/culture_news_reader/cli.rb
@@ -22,38 +22,22 @@ class CultureNewsReader::CLI
 			when "1"
 				#puts "Music News..."
 				music = Scraper.scrape_page('music')
-				music.each_with_index do |article, i|
-					puts "#{i+1}. #{article[:title]}"
-					puts "#{article[:description]}"
-					puts "---------------------------------------"
-				end
+				display(music)
 				#puts music
 				launcher(music)
 			when "2"
 				#puts "Style News..."
 				style = Scraper.scrape_page('style')
-				style.each_with_index do |article, i|
-					puts "#{i+1}. #{article[:title]}"
-					puts "#{article[:description]}"
-					puts "---------------------------------------"
-				end
+				display(style)
 				launcher(style)
 			when "3"
 				culture = Scraper.scrape_page('culture')
-				culture.each_with_index do |article, i|
-					puts "#{i+1}. #{article[:title]}"
-					puts "#{article[:description]}"
-					puts "---------------------------------------"
-				end
+				display(culture)
 				launcher(culture)
 			when "4"
 				trending = Scraper.scrape_trending_stories('trending')
-				trending.each_with_index do |article, i|
-					puts "#{i+1}. #{article[:title]}"
-					puts "#{article[:description]}"
-					puts "---------------------------------------"
-				end
-					launcher(trending)
+				display(trending)
+				launcher(trending)
 			when "list"
 				list_categories
 			end
@@ -69,6 +53,13 @@ class CultureNewsReader::CLI
 		Launchy.open(site_to_open)
 	end
 
+	def display(article_hash)
+		article_hash.each_with_index do |article, i|
+			puts "#{i+1}. #{article[:title]}"
+			puts "#{article[:description]}"
+			puts "---------------------------------------"
+		end
+	end
 
 
 end
